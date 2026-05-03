c04535c6563eb6c29a1ba5c6fe066aba7bd49450
Matthew Dinh <72724975+feenix1@users.noreply.github.com>
2026-05-02 21:57:38 -0700

    working on adding map

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { APIProvider } from "@vis.gl/react-google-maps";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,9 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <APIProvider apiKey={process.env.GOOGLE_MAPS_API_KEY!}>
+        <body className="min-h-full flex flex-col">{children}</body>
+      </APIProvider>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -24,23 +24,24 @@ export default function Home() {
     }
   }
 
-  //return the validation 
   return (
     <>
       <div className="flex flex-col items-center justify-center font-serif p-4">
         <h1 className="text-3xl font-bold">Infolection</h1>
       </div>
-      <div className="flex flex-col flex-1 items-center justify-start font-serif">
+      <div className="flex flex-col flex-1 items-center justify-center font-serif">
         <h1 className="text-4xl">Understand what's on your ballot.</h1>
         <input type="text" 
           placeholder="Enter your address..."
           value={address}
           onChange={(e) => setAddress(e.target.value)}
-          className="border border-gray-300 rounded-md p-2 mt-4 w-full max-w-md" 
+          className="border border-gray-300 rounded-md p-2 mt-4 w-full max-w-md outline-none focus:ring-2 focus:ring-stone-800"
         />
+        <button onClick={handleSubmit} className="bg-stone-800 text-zinc-200 px-4 py-2 rounded-md mt-4">
+          Get My Ballot
+        </button>
+        {error && <p className="text-red-500 mt-2">{error}</p>}
       </div>
     </>
   )
-
-
 }
